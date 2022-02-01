ccefa768f02d04eb6fb38be276455c5d6f3a3a49
jifakir <jifakir@outlook.com>
2022-02-01 12:35:20 +0600

    Updated

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function startApolloServer(typeDefs, resolvers) {
   });
 
   // Modified server startup
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise(resolve => httpServer.listen({ port: ProcessingInstruction.env.PORT || 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
